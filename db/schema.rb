9860c2cad16ff535be26ebeea035528544a3e6ab
Brian Parks <bpparks@gmail.com>
2012-07-30 15:47:34 -0700

    modified schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120730221859) do
+ActiveRecord::Schema.define(:version => 20120730223551) do
 
   create_table "answers", :force => true do |t|
     t.string   "body"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(:version => 20120730221859) do
     t.string   "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "editlink"
   end
 
   create_table "questions", :force => true do |t|
